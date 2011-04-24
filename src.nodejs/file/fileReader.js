21b992d6cd7b992243ecd1e97aaab954d8c52c49
booo <borgers@mi.fu-berlin.de>
2011-04-24 14:05:45 +0200

    get nodes in out of denseNodes; remove whitespaces

--- src.nodejs/file/fileReader.js
+++ src.nodejs/file/fileReader.js
@@ -17,6 +17,13 @@ var blobHeaderBlock = osmSchema['OSMPBF.HeaderBlock'];
 var fileName = './berlin.osm.pbf';
 
 var blobPrimitiveBlock = osmSchema['OSMPBF.PrimitiveBlock'];
+var primitiveGroup = osmSchema['OSMPBF.PrimitiveBlock'];
+console.log(primitiveGroup);
+var node = osmSchema['OSMPBF.Node'];
+var way = osmSchema['OSMBPF.Way'];
+var relation = osmSchema['OSMPBF.Relation'];
+var info = osmSchema['OSMPBF.Info'];
+
 
 var fs = require('fs');
 //fs.open(fileName,'r',start);
@@ -43,62 +50,95 @@ var blobHeaderSizeCallback = function(error, bytesRead, buffer){
 					console.log(blobHeader.parse(buffer));
 					var aBlobHeader = blobHeader.parse(buffer);
 					if(aBlobHeader.indexdata) {
-						console.log(indexdata);
+						//console.log(indexdata);
 					}
 					//read a blob from file
 					var tmp = new Buffer(aBlobHeader.datasize);
 					fs.read(fd,tmp,0,aBlobHeader.datasize,null,function(error, bytesRead, buffer){
 						if(error) {
-							console.log(error);	
+							console.log(error);
 						}
 						else {
 							if(bytesRead == aBlobHeader.datasize) {
 								var aBlob = blob.parse(buffer);
-								console.log(aBlob);
+								//console.log(aBlob);
 								var data = aBlob.raw;
 							    if(!data) {
 									if(aBlob.zlibData) {
 										data = require('zlib').inflate(aBlob.zlibData);
-											
+
 										//var gunzip = new require('compress').Gunzip();
 										//gunzip.write(aBlob.zlibData, function(error, data) {
 										//	console.log(data);
 										//});
-									} 
-									else if(aBlob.lzmaData) {}	
+									}
+									else if(aBlob.lzmaData) {}
 									else {
 										//var bunzip = new require('compress').Bunzip();
-										//bunzip.write(aBlob.bzipData2, function(error, data) {	
-										//});	
-									}	
-								}	
+										//bunzip.write(aBlob.bzipData2, function(error, data) {
+										//});
+									}
+								}
 								if(aBlobHeader.type === 'OSMHeader') {
 									var aBlobHeaderBlock = blobHeaderBlock.parse(data);
-									console.log(aBlobHeaderBlock);	
-								} 
+									//console.log(aBlobHeaderBlock);
+								}
 								else if(aBlobHeader.type === 'OSMData') {
-									var aBlobPrimitiveBlock = blobPrimitiveBlock.parse(data);	
+									var aBlobPrimitiveBlock = blobPrimitiveBlock.parse(data);
+									//console.log(aBlobPrimitiveBlock);
+									for(var i in aBlobPrimitiveBlock.primitivegroup){
+										debugger;
+										var aPrimitiveGroup = aBlobPrimitiveBlock.primitivegroup[i];
+										//console.log(aPrimitiveGroup);
+										if(aPrimitiveGroup.dense) {
+											//console.log(aPrimitiveGroup.dense);
+											var nodes = aPrimitiveGroup.dense;
+											var j;
+											for(j in nodes.id) {
+												//loop through node ids
+												//create nodes
+												//console.log(aPrimitiveGroup.dense.id[j]);
+												var aNode = {
+													id : nodes.id[j],
+													lat : nodes.lat[j],
+													lon : nodes.lon[j]
+												};
+												//TODO emit node
+												console.log(aNode);
+											}
+										}
+										//else {} perhaps use else instead?
+										var j;
+										for(j in aPrimitiveGroup.nodes) {
+											var aNode = {
+												id : aPrimitiveGroupe.nodes[j].id
+											};
+											//TODO emit node
+										}
+									}
+									//console.log(blobPrimitiveBlock.parse(aBlobPrimitiveBlock.primitivegroup));
 								}
 								else {
-									console.log('unsupported BlobHeader.type');	
-								}		
-								//console.log(data);	
+									console.log('unsupported BlobHeader.type');
+								}
+								//console.log(data);
 							}
 							else {
 								console.log('bytesRead != aBlobHeader.datasize');
 							}
 							//start next interation
-							fs.read(fd, new Buffer(4),0,4,null,blobHeaderSizeCallback);	
-						}		
-					});		
+							debugger; //just for debugging ;)
+							fs.read(fd, new Buffer(4),0,4,null,blobHeaderSizeCallback);
+						}
+					});
 				}
 				else {
-					console.log('bytesRead != blobHeaderSize');	
+					console.log('bytesRead != blobHeaderSize');
 				}
 					});
 		}
 		else {
-			console.log('could not read blobHeaderSize');	
+			console.log('could not read blobHeaderSize');
 		}
 	}
 };
@@ -107,14 +147,14 @@ var start = function(err, fd2) {
 	var tmp = new Buffer(4);
 	if(err) {
 		console.log(err);
-		return;	
+		return;
 	}
 	else {
 		fd=fd2;
-		fs.read(fd,tmp,0,4,null,blobHeaderSizeCallback);	
-	}	
+		fs.read(fd,tmp,0,4,null,blobHeaderSizeCallback);
+	}
 };
 var blobHeaderCallback = function(error, bytesRead, buffer) {
-	
+
 }
 fs.open(fileName,'r',start);
